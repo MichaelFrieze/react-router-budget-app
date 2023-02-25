60bfa50cd0762018a4b821195d29410bd483188a
Michael Frieze <michaelfrieze@icloud.com>
2023-02-24 21:07:16 -0500

    layout and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// Layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+// Actions
+import { logoutAction } from "./actions/logout";
 
 // Routes
-import Dashboard, { dashboardLoader } from './pages/Dashboard';
-import Error from './pages/Error';
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Dashboard />,
-    loader: dashboardLoader,
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />
+      },
+      {
+        path: "logout",
+        action: logoutAction
+      }
+    ]
   },
 ]);
 
 function App() {
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <div className="App">
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
